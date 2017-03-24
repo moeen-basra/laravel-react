e4486386b8703d7c474a1002abef9d725613073c
Rehan Manzoor <caprichano@gmail.com>
2017-03-24 17:58:01 +0400

    Added axios interceptor for handle web middleware

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,6 +3,13 @@ import { render } from "react-dom"
 
 import Routes from './routes/routes'
 
+window.axios = require('axios');
+
+window.axios.defaults.headers.common = {
+    'X-CSRF-TOKEN': window.Laravel.csrfToken,
+    'X-Requested-With': 'XMLHttpRequest'
+};
+
 render(
     Routes,
     document.getElementById('app')
