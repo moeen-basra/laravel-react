1830692af67cdd362e6114e658b03334a77a8526
Moeen Basra <m.basra@live.com>
2017-11-07 00:15:20 +0400

    minor change

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -5,6 +5,7 @@ use App\Article;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ArticleRequest;
 use App\User;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -12,7 +13,8 @@ class ArticleController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return LengthAwarePaginator|mixed
      */
     public function index(Request $request)
     {
