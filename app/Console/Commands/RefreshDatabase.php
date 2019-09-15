c7547c0dd47aba258a68929af7818a8872ae76b4
Moeen Basra <m.basra@live.com>
2019-09-15 14:38:21 +0400

    update refresh database cron

--- app/Console/Commands/RefreshDatabase.php
+++ app/Console/Commands/RefreshDatabase.php
@@ -40,39 +40,6 @@ class RefreshDatabase extends Command
     {
         $this->call('migrate:refresh');
         $this->call('db:seed');
-        $this->call('passport:install');
-
-        $personal_client_secret = DB::table('oauth_clients')->find(1)->secret;
-        $password_client_secret = DB::table('oauth_clients')->find(2)->secret;
-
-        $this->writeNewEnvironmentFileWith('PERSONAL_CLIENT_SECRET', $personal_client_secret);
-        $this->writeNewEnvironmentFileWith('PASSWORD_CLIENT_SECRET', $password_client_secret);
-    }
-
-    /**
-     * Write a new environment file with the given key.
-     *
-     * @param  string  $key
-     * @return void
-     */
-    protected function writeNewEnvironmentFileWith($key, $value)
-    {
-        file_put_contents($this->laravel->environmentFilePath(), preg_replace(
-            $this->keyReplacementPattern($key, $value),
-            "{$key}={$value}",
-            file_get_contents($this->laravel->environmentFilePath())
-        ));
-    }
-
-    /**
-     * Get a regex pattern that will match env APP_KEY with any random key.
-     *
-     * @return string
-     */
-    protected function keyReplacementPattern($key, $value)
-    {
-        $escaped = preg_quote("=".env($key), '/');
-
-        return "/^{$key}{$escaped}/m";
+        $this->call('passport:configure');
     }
 }
