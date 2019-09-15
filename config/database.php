bfe067bd716c017dff610888b5c6a3f56aea462e
Moeen Basra <m.basra@live.com>
2019-09-15 14:03:29 +0400

    update test dbname

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
         'testing' => [
             'driver'    => 'mysql',
             'host'      => env('DB_TEST_HOST', 'localhost'),
-            'database'  => env('DB_TEST_DATABASE', 'homestead_test'),
+            'database'  => env('DB_TEST_DATABASE', 'testing'),
             'username'  => env('DB_TEST_USERNAME', 'homestead'),
             'password'  => env('DB_TEST_PASSWORD', 'secret'),
             'charset'   => 'utf8',
