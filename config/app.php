d1280435a4bb2b982075d78118d3c459a44b9f9f
Moeen Basra <m.basra@live.com>
2020-08-08 00:54:45 +0500

    fix the password

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laravel 5.5 & ReactJS 16 Boilerplate',
+    'name' => 'Laravel 7 & ReactJS 16 Boilerplate',
 
     /*
     |--------------------------------------------------------------------------
