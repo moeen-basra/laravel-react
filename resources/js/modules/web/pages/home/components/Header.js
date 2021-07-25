29e2a40b2103f5a2e83d248f9d6f6590c9b3f0a0
Moeen Basra <m.basra@live.com>
2021-07-26 00:34:48 +0500

    update schema

--- resources/js/modules/web/pages/home/components/Header.js
+++ resources/js/modules/web/pages/home/components/Header.js
@@ -6,12 +6,6 @@ export default function Header() {
       <img width="125" height="125" src="https://graph.facebook.com/1243067599/picture?type=square" alt="..." className="rounded-circle" />
       <h1>Moeen Farooq Basra</h1>
       <p className="lead">Master in Information Technology</p>
-      <p className="lead">Fullstack Developer at&nbsp;
-        <a className="text-white"
-           href="http://ajar.ae"
-           target="_blank"
-           rel="noreferrer noopener">Ajar.ae</a>
-      </p>
       <p className="lead"><i className="fa fa-heart text-danger" />{`{ PHP, JavaScript, Python, MySQL, MongoDB }`}</p>
     </div>
   </header>
